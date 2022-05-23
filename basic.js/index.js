7c4cb72ba28dd04cd422fcab695325872c76bc26
Lalit Chauhan <84916022+Lalit-chauhan@users.noreply.github.com>
2022-05-23 07:48:11 +0530

    Adding small files

---
+++ basic.js/index.js
@@ -0,0 +1,10 @@
+function Print() {
+    console.log("I am Prateek....");
+    console.log("Here we are learning JS.");
+  }
+  
+  function Add(v1, v2) {
+    console.log("Output is:", v1 + v2);
+  }
+  
+  Add(3, 5);
